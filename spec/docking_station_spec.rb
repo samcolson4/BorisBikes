6707c8b62ba2b2dd9ed135a13d7562c007526a06
samcolson4 <samcolson4@gmail.com>
2020-09-03 16:30:37 +0100

    Added tests and methods for seeing a docked bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,7 +10,23 @@ describe DockingStation do
       expect(bike.working?).to eq true
     end
 
-    context "Docks a bike at a docking station" 
+    context "Docks a bike at a docking station"
       it { is_expected.to respond_to(:dock).with(1).argument }
+
+    context "It is expected to respond to a bike"
+      it { is_expected.to respond_to(:bike) }
+
+    it 'docks something' do
+      bike = Bike.new
+      station = DockingStation.new
+      expect(station.dock(bike)).to eq bike
+    end
+
+    it 'returns a docked bike' do
+      bike = Bike.new
+      station = DockingStation.new
+      station.dock(bike)
+      expect(station.bike).to eq bike
+    end
 
   end
